d51c6a69be19a590e18b8bf5ef64fca1be76ae23
Mohammad Maheer <43739258+mohammadmaheer@users.noreply.github.com>
2023-10-21 15:28:34 +0500

    whats app chat ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,167 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("WhatsApp"),
+//           backgroundColor: Color.fromARGB(176, 26, 255, 18),
+//         ),
+//         body: SingleChildScrollView(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               getChatUI(
+//                 "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg",
+//                 Colors.green,
+//                 "SA",
+//                 "Sabeel Ahmed",
+//                 "Where are you..?",
+//                 "10;20",
+//               ),
+//               getChatUI(
+//                 "https://www.rimage.com/emea/wp-content/themes/rimage/assets/images/disc.png",
+//                 Colors.yellow,
+//                 "AB",
+//                 "Abdullah Bin Shahid",
+//                 "Taking class right now",
+//                 "11:00",
+//               ),
+//               getChatUI(
+//                 "https://i0.wp.com/cocolapinedesign.com/wp-content/uploads/rimage-11-1.jpeg?fit=2000%2C1498&ssl=1",
+//                 Colors.red,
+//                 "AY",
+//                 "Ayesha",
+//                 "Where are you..?",
+//                 "01:20",
+//               )
+
+//               // Image.asset("assets/car1.webp"),
+//               // Image.asset("assets/car2.webp"),
+//               // Image.network(
+//               //     "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg"),
+//               // getBox(
+//               //     "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg"),
+//               // getBox(
+//               //     "https://images.unsplash.com/photo-1616455579100-2ceaa4eb2d37?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym13JTIwY2FyfGVufDB8fDB8fHww&w=1000&q=80"),
+//               // getBox(
+//               //     "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg"),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// getBox(String url) {
+//   return Container(
+//     height: 200,
+//     width: 200,
+//     decoration: BoxDecoration(
+//         image: DecorationImage(
+//           fit: BoxFit.cover,
+//           image: NetworkImage("$url"),
+//         ),
+//         border: Border.all(color: Colors.black, width: 5),
+//         color: Colors.yellow,
+//         borderRadius: BorderRadius.all(Radius.circular(500))),
+//   );
+// }
+
+getChatUI(String url, Color cColor, String avatar, String title,
+    String subTitle, String trailing) {
+  return ListTile(
+      tileColor: Color.fromARGB(255, 232, 232, 232),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(url),
+        backgroundColor: cColor,
+        child: Text(
+          "$avatar",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      title: Text("$title"),
+      subtitle: Text("$subTitle"),
+      trailing: Text("$trailing"));
+}
+
+getStatusUI(String url, Color cColor, String avatar, String title,
+    String subTitle, String trailing) {
+  return ListTile(
+      tileColor: Color.fromARGB(255, 232, 232, 232),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(url),
+        backgroundColor: cColor,
+        child: //Container(
+            //   decoration: BoxDecoration(
+            //       image: DecorationImage(
+            //         fit: BoxFit.cover,
+            //         image: NetworkImage(url),
+            //       ),
+            //       border: Border.all(color: Colors.black, width: 1),
+            //       color: Colors.yellow,
+            //       borderRadius: BorderRadius.all(Radius.circular(500))),
+            // ),
+
+            Text(
+          "$avatar",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      title: Text("$title"),
+      subtitle: Text("$subTitle"),
+      trailing: Text("$trailing"));
+}
+
+getCallUI(String url, Color cColor, String avatar, String title,
+    String subTitle, String trailing) {
+  return ListTile(
+    tileColor: Color.fromARGB(255, 232, 232, 232),
+    leading: CircleAvatar(
+      backgroundImage: NetworkImage(url),
+      backgroundColor: cColor,
+      child: //Container(
+          //   decoration: BoxDecoration(
+          //       image: DecorationImage(
+          //         fit: BoxFit.cover,
+          //         image: NetworkImage(url),
+          //       ),
+          //       border: Border.all(color: Colors.black, width: 1),
+          //       color: Colors.yellow,
+          //       borderRadius: BorderRadius.all(Radius.circular(500))),
+          // ),
+
+          Text(
+        "$avatar",
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+        ),
+      ),
+    ),
+    title: Text("$title"),
+    subtitle: Text("$subTitle"),
+    trailing: Icon(
+      Icons.call,
+      color: Colors.blue,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,115 +171,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Whats App UI',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // is not restarted.
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyCustomTab(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyCustomTab extends StatefulWidget {
+  const MyCustomTab({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyCustomTab> createState() => _MyCustomTabState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyCustomTabState extends State<MyCustomTab> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("WhatsApp"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.zero, color: Colors.grey[300]),
+                child: TabBar(
+                  indicator: BoxDecoration(
+                    color: Color.fromARGB(255, 167, 226, 236),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelColor: Colors.black,
+                  dividerColor: Colors.black,
+                  tabs: [
+                    Column(
+                      children: [
+                        Tab(
+                          icon: Icon(
+                            Icons.chat,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.video_call,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.call,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(children: [
+                  getChatUI(
+                    "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg",
+                    Colors.green,
+                    "SA",
+                    "Umar ali",
+                    "Helo..?",
+                    "05:14",
+                  ),
+                  getStatusUI(
+                    "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg",
+                    Colors.orange,
+                    "SA",
+                    "Waqar khan",
+                    "are you there..?",
+                    "23:10",
+                  ),
+                  getCallUI(
+                    "https://hips.hearstapps.com/hmg-prod/images/2024-lamborghini-revuelto-127-641a1d518802b.jpg",
+                    Colors.grey,
+                    "SA",
+                    "Saad Ahmed",
+                    "Where are you..?",
+                    "11:40",
+                  ),
+                ]),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
